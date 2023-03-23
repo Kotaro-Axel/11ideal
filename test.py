3ae9e01570920f1cd8e597841456e0f690461505
Kotaro-Axel <191246@ids.upchiapas.edu.mx>
2023-03-22 21:08:51 -0600

    AG update

--- test.py
+++ test.py
@@ -79,14 +79,32 @@ def mutacion(equipo, probabilidad_mutacion):
         equipo.insert(1, generar_jugador().get("atributos"))
     return equipo
 
-        
+def diferencias(generaciones):
+    poblacionPt = generaciones[0].copy()
+    poblacionDf = generaciones[0].copy()
+    poblacionMd = generaciones[0].copy()
+    poblacionDl = generaciones[0].copy()
 
+    mejores = []
+    evaluacionesPorteros = [jugador_mas_similar(poblacionPt, JugadorIdeal.Portero) for jugador in range(4)]
+    evaluacionesDenfensas = [jugador_mas_similar(poblacionDf, JugadorIdeal.Defensa) for jugador in range(4)]
+    evaluacionesMedios = [jugador_mas_similar(poblacionMd, JugadorIdeal.Medio) for jugador in range(4)]
+    evaluacionesDelanteros = [jugador_mas_similar(poblacionDl, JugadorIdeal.Delantero) for jugador in range(4)]
+    
+    mejores.append(evaluacionesPorteros)
+    mejores.append(evaluacionesDenfensas)
+    mejores.append(evaluacionesMedios)
+    mejores.append(evaluacionesDelanteros)
+
+    return mejores
         
 
 def genetico(probabilidad_mutacion, num_generaciones):
 
+    generaciones = []
+
+
     for generacion in range(num_generaciones):
-        print("Generacion: ", generacion)
         #Seleccion
         seleccionados = seleccion()
         #print(seleccionados,"*")
@@ -102,6 +120,9 @@ def genetico(probabilidad_mutacion, num_generaciones):
             nueva_poblacion[i] = mutacion(nueva_poblacion[i], probabilidad_mutacion)
         #Poda
         Poblacion = seleccionados[0] + seleccionados[1] + nueva_poblacion[1] + nueva_poblacion[0]
+        generaciones.append(Poblacion)
+    
+    primera_generacion = diferencias(generaciones)
     
     PoblacionParaEvaluarPorteros = Poblacion.copy()
     PoblacionParaEvaluarDefensas = Poblacion.copy()
@@ -119,9 +140,11 @@ def genetico(probabilidad_mutacion, num_generaciones):
     mejores.append(evaluacionesMedios)
     mejores.append(evaluacionesDelanteros)
 
-    return mejores
+    return [mejores,primera_generacion]
 
 
 PROBABILIDAD_MUTACION = 0.5
 NUM_GENERACIONES = 100
-# mejores_jugadores = genetico( PROBABILIDAD_MUTACION, NUM_GENERACIONES)
+mejores_jugadores = genetico( PROBABILIDAD_MUTACION, NUM_GENERACIONES)
+print(mejores_jugadores)
+
