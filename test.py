7d113a329f8b00cc4569596dd03ae89b2181873c
Kotaro-Axel <191246@ids.upchiapas.edu.mx>
2023-03-24 22:50:57 -0600

    aptitud update

--- test.py
+++ test.py
@@ -22,22 +22,36 @@ def generar_jugador():
             jugador[posicion][caracteristica] = np.round(random.uniform(0.70, 0.99),2)
     return jugador.get("atributos")
 
-def aptitud(poblacion, jugador_ideal):
-    similitud_max = -math.inf
-    jugador_mas_similar = None
-    for equipo in poblacion:
-        indice = 0
-        for jugador in equipo:
-            similitud = 0
-            for caracteristica in GENES:
-                    similitud += abs(min(jugador[caracteristica] - jugador_ideal[caracteristica], 0))
-            if similitud > similitud_max:
-                similitud_max = similitud
-                print(similitud)
-                jugador_mas_similar = jugador
-                equipo.pop(indice)
-            indice+=1
-    return jugador_mas_similar
+def aptitud(equipo, defensaideal, medioideal, delanteroideal, porteroideal):
+
+    similitud_global = []  
+
+    for defensas in equipo[:4]:
+        diferencia = 0
+        for caracteristica in GENES:
+            diferencia += abs(min(defensas[caracteristica] - defensaideal[caracteristica], 0))
+        similitud_global.append(diferencia)
+
+    for medios in equipo[4:8]:
+        diferencia = 0
+        for caracteristica in GENES:
+            diferencia += abs(min(medios[caracteristica] - medioideal[caracteristica], 0))
+        similitud_global.append(diferencia)
+
+    for delanteros in equipo[8:10]:
+        diferencia = 0
+        for caracteristica in GENES:
+            diferencia += abs(min(delanteros[caracteristica] - delanteroideal[caracteristica], 0))
+        similitud_global.append(diferencia)
+
+
+    for portero in equipo[10:11]:
+        diferencia = 0
+        for caracteristica in GENES:
+            diferencia += abs(min(portero[caracteristica] - porteroideal[caracteristica], 0))
+        similitud_global.append(diferencia)
+
+    return sum(similitud_global)
 
 def jugador_mas_similar(jugadores, jugador_ideal):
     similitud_max = -math.inf
@@ -67,9 +81,25 @@ def generacionIndividual(cantidad, tam):
     return equipos
 
 
-def seleccion():
-    conjunto1 = Poblacion[(random.randint(0, len(Poblacion)-1))]
-    conjunto2 = Poblacion[(random.randint(0, len(Poblacion)-1))]
+def seleccion(Poblacion):
+
+    individuos = []
+    individuo = 0
+
+    for equipo in Poblacion:
+        individuo = aptitud(equipo, Modelo.Defensa, Modelo.Medio, Modelo.Delantero, Modelo.Portero)
+        individuos.append(individuo)
+
+    mejor = min(individuos)
+    indice_mejor = individuos.index(mejor)
+    conjunto1=Poblacion[indice_mejor]
+
+    individuos.pop(indice_mejor)
+
+    mejor = min(individuos)
+    indice_mejor = individuos.index(mejor)
+    conjunto2 = Poblacion[indice_mejor]
+
     return conjunto1, conjunto2 
 
 def cruza(padre1,padre2):
@@ -114,7 +144,7 @@ def genetico(probabilidad_mutacion, num_generaciones, Poblacion):
 
     for generacion in range(num_generaciones):
         #Seleccion
-        seleccionados = seleccion()
+        seleccionados = seleccion(Poblacion)
         #print(seleccionados,"*")
         
         #Cruza
@@ -129,31 +159,33 @@ def genetico(probabilidad_mutacion, num_generaciones, Poblacion):
         #Poda
         Poblacion = [madre, padre , hijo[1] , hijo[0]]
         generaciones.append(Poblacion)
-
-    print(len(Poblacion))
-    # primera_generacion = diferencias(generaciones)
     
-    PoblacionParaEvaluarPorteros = Poblacion.copy()
-    # PoblacionParaEvaluarDefensas = Poblacion.copy()
-    # PoblacionParaEvaluarMedios = Poblacion.copy()
-    # PoblacionParaEvaluarDelanteros = Poblacion.copy()
+    aprendizaje = []
 
-    # mejores = []
-    evaluacionesPorteros = [aptitud(PoblacionParaEvaluarPorteros, JugadorIdeal.Portero) for jugador in range(4)]
-    # evaluacionesDenfensas = [jugador_mas_similar(PoblacionParaEvaluarDefensas, JugadorIdeal.Defensa) for jugador in range(4)]
-    # evaluacionesMedios = [jugador_mas_similar(PoblacionParaEvaluarMedios, JugadorIdeal.Medio) for jugador in range(4)]
-    # evaluacionesDelanteros = [jugador_mas_similar(PoblacionParaEvaluarDelanteros, JugadorIdeal.Delantero) for jugador in range(4)]
-    
-    # mejores.append(evaluacionesPorteros)
-    # mejores.append(evaluacionesDenfensas)
-    # mejores.append(evaluacionesMedios)
-    # mejores.append(evaluacionesDelanteros)
-    for portero in evaluacionesPorteros:
-        print (portero)
-    # return [mejores,primera_generacion]
+    for poblacion in generaciones:
+        aptitudEquipo = 0
+        for equipo in poblacion:
+            aptitudEquipo = aptitud(equipo, Modelo.Defensa, Modelo.Medio, Modelo.Delantero, Modelo.Portero)
+        aprendizaje.append(aptitudEquipo)
+
+
+    mejor = min(aprendizaje)
+    mejor_generacion = aprendizaje.index(mejor)
+
+    UltimaGeneracion = Poblacion.copy()
+
+    for equipo in UltimaGeneracion:
+        aptitud(equipo, Modelo.Defensa, Modelo.Medio, Modelo.Delantero, Modelo.Portero)
+
+
+    return [mejor_generacion,aprendizaje]
 
 Poblacion = generacionIndividual(4,20)
 PROBABILIDAD_MUTACION = 0.5
-NUM_GENERACIONES = 1
+NUM_GENERACIONES = 20
+
 mejores_jugadores = genetico( PROBABILIDAD_MUTACION, NUM_GENERACIONES, Poblacion)
+
+mejor_generacion = mejores_jugadores[0]
+curva_aprendizaje = mejores_jugadores[1]
 
